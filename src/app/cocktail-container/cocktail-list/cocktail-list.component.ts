04809ecc100cd814831612cb6353be3574db1f09
SebastienDms <sebastien.damsin@gmail.com>
2022-07-06 21:38:02 +0200

    Group components cocktail in a container

---
+++ src/app/cocktail-container/cocktail-list/cocktail-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Cocktail } from 'src/app/interfaces/cocktail.interface';
+
+@Component({
+  selector: 'app-cocktail-list',
+  templateUrl: './cocktail-list.component.html',
+  styleUrls: ['./cocktail-list.component.scss'],
+})
+export class CocktailListComponent implements OnInit {
+  @Input() public cocktails?: Cocktail[];
+  @Output() private changeCocktail: EventEmitter<number> = new EventEmitter();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public selectCocktail(index: number): void {
+    console.log(index);
+    this.changeCocktail.emit(index);
+  }
+}
