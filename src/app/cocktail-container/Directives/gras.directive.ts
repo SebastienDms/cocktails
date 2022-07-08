c21b5a2db4aceb0d54246f5f4da67483c9f91b6d
SebastienDms <sebastien.damsin@gmail.com>
2022-07-08 12:01:33 +0200

    Test Hostlistener

--- src/app/cocktail-container/Directives/gras.directive.ts
+++ src/app/cocktail-container/Directives/gras.directive.ts
@@ -1,14 +1,31 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[elementGras]',
 })
 export class ElementGras implements OnInit {
-  constructor(private element: ElementRef<any>) {
-    console.log(element);
+  @HostListener('mouseenter', ['$event']) private colorerJaune(
+    event: MouseEvent
+  ): void {
+    console.log('Listener : ' + this.el);
+    console.log('Event : ' + event);
+
+    this.el.nativeElement.style.color = 'yellow';
+  }
+  @HostListener('mouseleave', ['$event']) private colorerNoir(
+    event: MouseEvent
+  ): void {
+    console.log('Listener : ' + this.el);
+    console.log('Event : ' + event);
+
+    this.el.nativeElement.style.color = 'black';
+  }
+
+  constructor(private el: ElementRef<any>) {
+    console.log(el);
   }
 
   ngOnInit(): void {
-    this.element.nativeElement.style.fontWeight = 'bold';
+    this.el.nativeElement.style.fontWeight = 'bold';
   }
 }
